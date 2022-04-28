e4acc55f9112984d20e1023804dcaf3b9fd11da8
pablo575 <maicolysofia55@gmail.com>
2022-04-28 09:28:10 -0500

    nuevo comit

---
+++ pagi.js/pagi.js
@@ -0,0 +1,121 @@
+/* operadores logicos and 
+todos son negaticos menos verdadero verdadero* /
+/*let num1 = true;
+let num2 = false;
+let num3 = num1 && num2;
+console.log (num3) 
+respuesta falsa */
+/*
+let num1 = false;
+let num2 = true;
+let num3 = num1 && num2;
+console.log (num3);
+respuesta falsa*/
+
+/*let num1 = true;
+let num2 = true;
+num = num1 && num2;
+console.log(num)
+respuesta verdadera*/
+/*
+let num = false;
+let num1 = false;
+let num2 = num && num1;
+console.log(num2);
+respuesta falsa*/
+/* operadores or */
+/*
+let num1 = true;
+let num2 = true;
+let num3 = num1|| num2;
+console.log(num3)
+respuesta verdadera */
+/*
+let num = true;
+let num1 = false;
+let num2 = num || num1;
+console.log(num2)
+respuesta verdadera;*/
+/*
+let num = false;
+let num1 = true;
+let num2 = num||num1;
+console.log(num2)
+respuesta verdadera*/
+/*
+let num1 = false;
+let num2 = false;
+let num3= num1||num2;
+console.log(num3)
+respuesta verdadera */
+/* simbolo matematico modulo % es el siguiente numero entero del resultado de la division*/
+/*
+let num =10;
+let num1 =5;
+resultado = num % num1;
+console.log(resultado)*/
+/*
+let num = 9;
+let num1 = 5;
+resultado = num %num1;
+console.log(resultado)*/ 
+/*
+let num =5;
+num = num -1
+console.log(num)*/
+/* operadores relacionales  "<" menor que ">"mayor que ">="mayor igual que  "<="menor igual que "=="igual que "!=" distinto de.*/
+/*
+let numero = 3;
+let num = 5;
+restultado = numero< num;
+console.log(restultado)*/
+/*
+let num = 3;
+let numero1=5
+resultado =num  >numero1;
+console.log(resultado)*/
+/*
+let num  =3;
+let nu = 5;
+resultado = num == nu ;
+console.log(resultado)*/
+/*
+let num= 3;
+let mun =5;
+resultado = num != mun ;
+console.log(resultado)*/
+/*let num = 5;
+resultado =num =3;
+console.log(resultado)
+*//*
+let numeor =5;
+let nume = numeor==3;
+console.log(nume)*/
+/*
+let vari = "esto es un estrin";
+let vari1="esto es un estrin";
+let vari2="esto puede ser un estrin";
+resultado = vari !=vari2;
+console.log(resultado)*/
+/*
+let valores =[true,5, false, "hola", "adios", 2 ];
+let val = valores [1];
+let val1 = valores [5];
+respuesta =val % val1;
+console.log(respuesta)*/
+/*let numero = 5;
+let numero1 = 8;
+if (numero<numero1){
+    alert("numero uno es menor que numeor dos" )
+}*/
+/*
+let val =5;
+let val1 = 8;
+if(val1>=0){
+    alert("numero dos es positivo")
+}*//*
+let val =6;
+let val1 =8;
+if (val<val1){
+     alert("el numero es negativo")
+}*/
